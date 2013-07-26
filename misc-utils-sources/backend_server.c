c77e83a8ad8e506ebb0f19579786a84adc2e47b4
Huan Truong <htruong@tnhh.net>
2013-07-26 01:22:57 -0500

    Refine utilities/backend server

--- misc-utils-sources/backend_server.c
+++ misc-utils-sources/backend_server.c
@@ -6,6 +6,65 @@
 #include <event.h>
 #include <evhttp.h>
 
+#include <string.h>
+#include <stdio.h>
+
+char * escape(char*str) {
+    
+    char *escStr;
+    int i, count_final, count = strlen(str);
+    
+    count_final = count + 1;
+    
+    for(i=0; i<count; i++) {
+    	if (str[i] == '\'' || str[i] == '\n') {
+    			count_final++;
+    	}
+    } 
+
+    escStr = (char *) malloc(count_final * sizeof(char));
+
+    count_final = 0;
+	
+    for(i=0; i<count; i++) {
+    	if (str[i] == '\'' || str[i] == '\n') {
+    			escStr[count_final] = '\\';
+    			count_final++;
+    	}
+    	if (str[i] == '\n') {
+    		escStr[count_final] = 'n';
+    	} else {
+    		escStr[count_final] = str[i];
+    	}
+    	count_final++;
+    }
+    escStr[count_final] = '\0';
+
+    return escStr;
+}
+
+int exec(char* cmd, char * ret, int max) {
+    FILE* pipe = popen(cmd, "r");
+    int ret_val = 0;
+    if (!pipe) ret_val = 0;
+    //char buffer[128];
+    //while((!feof(pipe)) && (counter < max)) {
+    	//if(fgets(buffer, 128, pipe) != NULL) {
+    		//strncpy(&ret[counter], buffer, 128);
+    	//}
+    	//counter += 128;
+    //}
+    int max_remaining = max;
+    while (!feof(pipe)) {
+    	max_remaining = max - strlen(ret) - 1;
+    	if (fgets(&ret[strlen(ret)], max_remaining, pipe) == NULL) {
+    		break;
+	    }	
+    }
+    
+    pclose(pipe);
+    return ret_val;
+}
 
 void argtoi(struct evkeyvalq *args, char *key, int *val, int def)
 {
@@ -52,35 +111,54 @@ struct evbuffer *buf;
 void sys_handler(struct evhttp_request *req, void *arg)
 {
 	struct evbuffer *buf;
-	char cmd[255] = "";
+	char * cmd;
 	int valid_cmd = 1;
+
+
+	cmd = (char *) malloc(1024 * sizeof(char));
 
 	buf = evbuffer_new();
 	if (buf == NULL)
 		err(1, "failed to create response buffer");
 	evhttp_add_header(evhttp_request_get_output_headers(req),
 		    "Content-Type", "text/javascript");
-	//evbuffer_add_printf(buf, "Requested: %s id=%d parm=%s", evhttp_request_uri(req), _get_int(req, "id", -1), _get_chars(req, "parm", "NULL"));
+	//evbuffer_add_printf(buf, "Requested: %s id=%d parm=%s", evhttp_request_uri(req), _get_int(req, "id", -1), _get_chars(req, "parm", "NULL"));	
 
-	if (strncmp(_get_chars(req, "cmd", ""),"set_brightness") == 0) {
-		sprintf(cmd, "/bin/bash -c 'echo %d > /sys/devices/platform/stmp3xxx-bl/backlight/stmp3xxx-bl/brightness'", _get_int(req, "val", 0));
-		system(cmd);
-	} else if (strncmp(_get_chars(req, "cmd", ""),"shutdown") == 0) {
-		system("shutdown -P now");
+	if (strncmp(_get_chars(req, "cmd", ""),"set_brightness", 50) == 0) {
+		sprintf(cmd, "/bin/bash -c 'echo %d > /sys/devices/platform/stmp3xxx-bl/backlight/stmp3xxx-bl/brightness'", _get_int(req, "parm", 0));
+	} else if (strncmp(_get_chars(req, "cmd", ""),"shell", 50) == 0) {
+		sprintf(cmd, "/bin/bash -c '%s'", _get_chars(req, "parm", ""));
+	} else if (strncmp(_get_chars(req, "cmd", ""),"exec", 50) == 0) {
+		sprintf(cmd, "%s", _get_chars(req, "parm", ""));
 	} else {
 		valid_cmd = 0;
 	}
 
-	evbuffer_add_printf(buf, "{'result':%d}", valid_cmd);
+
+	if (valid_cmd) {
+		char * ret;
+		char * ret_escaped;
+		ret = (char *) malloc(4096 *sizeof(char));
+		exec(cmd, ret, 4096 *sizeof(char));
+		//fprintf(stderr, "%s\n", ret);
+		ret_escaped = escape(ret);
+		free(ret);
+		evbuffer_add_printf(buf, "{\"valid\":%d,\"result\":\"%s\"}", valid_cmd, ret_escaped);
+		free(ret_escaped);
+	} else {
+		evbuffer_add_printf(buf, "{\"valid\":%d,\"result\":\"\"}", valid_cmd);
+	}
 
 	evhttp_send_reply(req, HTTP_OK, "OK", buf);
+
+	free(cmd);
 }
 
 int main(int argc, char **argv)
 {
 	struct evhttp *httpd;
 	event_init();
-	httpd = evhttp_start("127.0.0.1", 8080);
+	httpd = evhttp_start(argv[argc-2], atoi(argv[argc-1]));
 	evhttp_set_cb(httpd, "/request_sys", sys_handler, NULL); 
 /* Set a callback for all other requests. */
 	evhttp_set_gencb(httpd, notfound_hander, NULL);
